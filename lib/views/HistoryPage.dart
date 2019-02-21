d879b482782f71e3e1913c785f5ff0d3f9ef0a28
Bybeez <green.link33@hotmail.fr>
2019-02-21 12:33:56 +0100

    separated listview from itembuilder

--- lib/views/HistoryPage.dart
+++ lib/views/HistoryPage.dart
@@ -5,18 +5,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HistoryPage extends StatelessWidget {
+  
+  Widget _buildBeaconItem(BuildContext context, int index){
+    return ListTile(
+          leading: Icon(Icons.location_on),
+          title: Text('Map'),
+        );
+  }
+  
   @override
   Widget build(BuildContext context) {
-
     return Container(
-      child: ListView(
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.location_on),
-            title: Text('Map'),
-          ),
-        ],
-      ),
+      child: ListView.builder(
+          itemBuilder: _buildBeaconItem,
+          itemCount: 1,
+      )          
     );
   }
 }
