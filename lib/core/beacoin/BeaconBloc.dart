b0856fc7ca02561db752d4a6efd114195698aa85
Farsen AC <farsen.abdallahcombo@ynov.com>
2019-02-21 09:27:07 +0100

    ✨ feat(beacon): introduce the beacon

---
+++ lib/core/beacoin/BeaconBloc.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:city_tips/core/beacoin/BeaconEvent.dart';
+import 'package:city_tips/core/beacoin/BeaconState.dart';
+import 'package:city_tips/core/repositories/BeacoinRepository.dart';
+import 'package:meta/meta.dart';
+
+
+class BeaconBloc extends Bloc<BeaconEvent, BeaconState>{
+    final BeaconRepository beaconRepository;
+
+  BeaconBloc({@required this.beaconRepository}): assert(beaconRepository != null);
+
+  @override
+  // TODO: implement initialState
+  BeaconState get initialState => BeaconUninitialized();
+
+  @override
+  Stream<BeaconState> mapEventToState(
+    BeaconState currentState, 
+    BeaconEvent event
+    )async * {
+      if(event is AppStarted){
+        yield BeaconSuccess();
+      }
+   
+  }
+}
