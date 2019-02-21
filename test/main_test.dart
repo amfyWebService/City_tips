27163d07ad8f7b6c077bccc383b3904895db6056
goms <dgomesh@gmail.com>
2019-02-21 12:11:40 +0100

    feat: start unit test

--- test/main_test.dart
+++ test/main_test.dart
@@ -12,6 +12,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:city_tips/main.dart';
 
 void main() {
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
 //    await tester.pumpWidget(App(userRepository: UserRepository()));
