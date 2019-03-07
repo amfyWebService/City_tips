5ada1d770faa07a7b339ac0d135184a6e06bc26a
Farsen AC <farsen.abdallahcombo@ynov.com>
2019-03-07 01:24:37 +0100

    chore: remove useless code

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -16,7 +16,6 @@ class _HomePageState extends State<HomePage> {
 
   List<MenuItem> menu = [
     MenuItem(title: "History", icon: Icons.history, page: HistoryPage()),
-    //MenuItem(title: "Beacon", icon: Icons.trip_origin, page: BeaconPage())
     MenuItem(title: "Settings", icon:Icons.settings_applications, page: SettingsPage()),
     MenuItem(title: "Map", icon:Icons.map, page: MapPage()),
   ];
